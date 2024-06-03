aebc2c2e0dd83f725507f12e7afdae3f5aa0588e
Daniel Shi <danielshi@Daniels-MacBook-Pro.local>
2024-06-03 01:03:32 -0700

    Updating image car1

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,9 @@ import React from "react";
 import "@splidejs/react-splide/css";
 import { LOOP } from "@splidejs/splide";
 import logo from "@/app/public/images/logos/white-logo.png";
-import test from "@/app/public/images/wzc4-mob-nav4.webp";
-import car1 from "@/app/public/images/pexels-james-wheeler-2782485.jpg";
+import test from "@/app/public/images/wzc4-mob-nav4.webp"
+import car1 from "@/app/public/images/pexels-james-wheeler-2782485.jpg"
+
 
 const content = [
   {
@@ -40,6 +41,7 @@ const content = [
   },
 ];
 
+
 function LandingDesktop() {
   return (
     <section>
@@ -63,18 +65,25 @@ function LandingDesktop() {
       >
         {content.map((item, index) => {
           return (
-            <SplideSlide
-              key={index}
-              className="lg:h-[700px] md:h-[500px] h-[400px]"
-            >
-              <ContentfulImage
-                className="d-block w-[80vw] h-100"
-                src={item.image}
+            <SplideSlide key={index} className="min-h-screen w-full">
+              {/* <ContentfulImage
+                className="d-block w-100 h-100"
+                src={require(`@/app/public${item.src}`)}
                 // fallbackSrc={item.fallback}
-                //   alt={item.alt}
+                alt={item.alt}
                 layout="fill"
                 objectFit="cover"
-              />
+                priority
+              /> */}
+              <ContentfulImage
+                  objectFit="cover"
+                  layout="fill"
+                  src={item.image}
+                  // src={test}
+                  // alt={item.alt}
+                  priority
+                  className={style["logo"]}
+                />
             </SplideSlide>
           );
         })}
