f3c0ebbdfdbbbeb9574050bff34d35b792eafca8
Daniel Shi <danielshi@Daniels-MacBook-Pro.local>
2024-07-01 22:37:07 -0700

    Updating articles

--- app/JSON/MenuItems.js
+++ app/JSON/MenuItems.js
@@ -1,9 +1,6 @@
-// import { ImLinkedin2 } from "react-icons/im";
-// import { FaFacebookF } from "react-icons/fa";
 import NavLogo from "../public/images/logos/nav-logo.png";
 import QR from "../public/images/qr.webp";
 import QRfb from "../public/images/qr.webp";
-// import ArticleListImage from "../../public/images/article/articlelist1.png";
 
 const Logo = NavLogo;
 const QRCode = QR;
